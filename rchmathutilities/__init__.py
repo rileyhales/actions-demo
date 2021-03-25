9179009f46a800aad1b539af2df3669fea70aac1
Riley Hales <rileyhales1@gmail.com>
2021-03-25 16:08:45 -0600

    push rchmathutils to trigger github actions

---
+++ rchmathutilities/__init__.py
@@ -0,0 +1,7 @@
+def square(x):
+    return x * x
+
+def add(*args):
+    return sum(args)
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rchmathutils',
+    packages=['rchmathutils'],
+    version='0.1',
+    description='simple math functions for demonstration purposes',
+    author='Riley Hales',
+    license='BSD 3-Clause Clear',
+    python_requires='>=3',
+    install_requires=['numpy', ],
+)
